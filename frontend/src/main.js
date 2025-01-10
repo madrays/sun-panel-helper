a6dd37941ca4b0093578f0c028a0ecf2bfccc9ee
madrays <keke210302@gmail.com>
2025-01-09 19:15:40 -0800

    fix: 添加 element-ui 依赖并修复 main.js 引用

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Root from './Root.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -9,5 +9,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(Root)
 }).$mount('#app')
